df57dcf1801f2371189ecf560a848c8671c4fd0d
Vadym <Sum.vadim415579suq@gmail.com>
2024-03-11 14:58:58 +0200

    Create a column with logo and texts

--- app/src/main/java/ua/lntu/edu/ua/cw1/MainActivity.kt
+++ app/src/main/java/ua/lntu/edu/ua/cw1/MainActivity.kt
@@ -3,13 +3,23 @@ package ua.lntu.edu.ua.cw1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ua.lntu.edu.ua.cw1.ui.theme.IPZ_CR_1Theme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    BusinessCard()
                 }
             }
         }
@@ -30,17 +40,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun BusinessCard(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Logo()
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = "Symonchuk Vadym",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.bodyLarge
+        )
+
+        Spacer(modifier = Modifier.height(23.dp))
+
+        // Опис
+        Text(
+            text = "Developer",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
+@Composable
+fun Logo() {
+    androidx.compose.foundation.Image(
+        painter = painterResource(id = R.drawable.android_logo), // ваш ресурс для логотипу
+        contentDescription = "Logo",
+        modifier = Modifier
+            .size(100.dp),
+        contentScale = ContentScale.Crop
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     IPZ_CR_1Theme {
-        Greeting("Android")
+        BusinessCard()
     }
 }
